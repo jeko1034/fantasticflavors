e0d4f44c7801fdda4353c166bfb767fd6b0b254b
Mark Carlton <mcarlton796@gmail.com>
2026-08-11 18:45:29 -0700

    many changes made by mark

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+let de_nav = document.getElementById("bars");
+document.querySelectorAll(".nav-items").forEach(el => el.addEventListener("click", navigation))
+
+de_nav.addEventListener("click", ShowHide);
+// console.log(nav_items);
+
+
+if (window.screen.width < 960){
+    Menu.style.display = "none";
+}
+
+function ShowHide(){
+    var Menu = document.getElementById("Menu");
+    if(de_nav.classList.contains("change")){
+        Menu.style.display = "none";
+        de_nav.classList.remove("change");
+    }else{
+        Menu.style.display = "block";
+        de_nav.classList.add("change");
+    }
+}
+
+function navigation(){
+    if(de_nav.classList.contains("change")){
+        Menu.style.display = "none";
+        de_nav.classList.remove("change");
+    }
+}
